6f3f65df66335ac6186c7e8df6b02c4867550497
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-08 21:52:10 -0500

    installed node and sql

---
+++ db/db.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE myReportSamples;
+USE myReportSamples;
---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE participants (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    participant VARCHAR(30) NOT NULL,
+    code VARCHAR(30) NOT NULL, 
+    date VARCHAR(30) NOT NULL, 
+    time VARCHAR(30) NOT NULL
+    );
+
